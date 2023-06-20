31dbda231940c0beba944e6ed97325b24f7b9327
lucas <lucasrds17@outlook.com>
2023-06-19 22:58:50 -0300

    commit

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,13 +1,20 @@
 import { Request, Response } from 'express';
+import { Op } from 'sequelize';
 import {sequelize} from '../instances/mysql';
 import { Product } from '../models/Product';
 import { User } from '../models/User';
 
 export const home = async(req: Request, res: Response)=>{
 
-    let users = await User.findAll();
+    let users = await User.findAll({
+        where: {
+            [Op.or]: [
+                {idade: 19}
+            ]
+        }
+    });
     console.log(JSON.stringify(users));
-
+    //sequelize documentation https://sequelize.org/docs/v6/core-concepts/model-querying-basics/
     let data: Record<string | number, any> = {};
     data['age'] = 90;
     data['showOld'] = false
